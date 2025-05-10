9867022a3eb8b0c7eff978e9def9121432f0f1bc
Keity Pinho <keity.de.moura.pinho@gmail.com>
2025-05-10 02:04:53 -0300

    correção do desafio

--- desafio.py
+++ desafio.py
@@ -1,3 +1,5 @@
+CONSTANTE_BONUS = 1000
+
 # 1) Solicita ao usuário que digite seu nome
 nome = input('Digite seu nome: ')
 
@@ -7,14 +9,13 @@ salario = float(input('Digite o valor do seu salário: '))
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-bonus = float(input('Digite o valor do seu bônus: '))
+bonus = float(input('Digite o valor do seu bônus (por exemplo "1.5"): '))
 
 # 4) Calcule o valor do bônus final
-resultado = salario + bonus
+resultado = CONSTANTE_BONUS + salario * bonus
 
 # 5) Imprima cálculo do KPI para o usuário
-print('\n')
 print(resultado)
 
 # 6) Imprime a mensagem personalizada incluindo o nome do usuário
-print(nome + ' o valor do seu bônus final será: ' + str(resultado))
+print(f'{nome} o valor do seu bônus final será: {resultado}')
